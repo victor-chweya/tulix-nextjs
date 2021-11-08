6d4b5c90ac01767b966bb588d8d74daba3fddd97
Victor <vchweya@avicdesign.com>
2021-11-08 13:45:56 +0300

    added blur to homepage

--- pages/index.js
+++ pages/index.js
@@ -67,7 +67,7 @@ export default function Home() {
 						</div>
 						<div className="row justify-content-center py-5">
 							<div className="col-lg-4 p-5 offset-xl-1 order-last">
-								<Image src="/img/beneficiary-mockup.png" alt="tulix beneficiary-mockup" className="img-fluid" height={567} width={283} />
+								<Image src="/img/beneficiary-mockup.png" alt="tulix beneficiary-mockup" className="img-fluid" placeholder="blur" height={567} width={283} />
 							</div>
 							<div className="col-lg-6 col-xl-6  d-flex flex-column justify-content-center ">
 								{/* <h6 class="text-success">Features</h6> */}
@@ -77,7 +77,7 @@ export default function Home() {
 						</div>
 						<div className="row justify-content-center py-5">
 							<div className="col-lg-4 p-5">
-								<Image src="/img/paybill-mockup.png" alt="tulix paybill-mockup" className="img-fluid" height={567} width={283} />
+								<Image src="/img/paybill-mockup.png" alt="tulix paybill-mockup" className="img-fluid" placeholder="blur" height={567} width={283} />
 							</div>
 							<div className="col-lg-6 col-xl-6 offset-xl-1 d-flex flex-column justify-content-center order-first order-lg-last">
 								{/* <h6 class="text-success">Features</h6> */}
@@ -87,7 +87,7 @@ export default function Home() {
 						</div>
 						<div className="row justify-content-center py-5">
 							<div className="col-lg-4 p-5 offset-xl-1 order-last">
-								<Image src="/img/transaction-mockup.png" alt="tulix transaction-mockup" className="img-fluid" height={567} width={283} />
+								<Image src="/img/transaction-mockup.png" alt="tulix transaction-mockup" className="img-fluid" placeholder="blur" height={567} width={283} />
 							</div>
 							<div className="col-lg-6 col-xl-6  d-flex flex-column justify-content-center ">
 								{/* <h6 class="text-success">Features</h6> */}
@@ -97,7 +97,7 @@ export default function Home() {
 						</div>
 						<div className="row justify-content-center py-5">
 							<div className="col-lg-4 p-5">
-								<Image src="/img/receipt-mockup.png" alt="tulix receipt-mockup" className="img-fluid" height={567} width={283} />
+								<Image src="/img/receipt-mockup.png" alt="tulix receipt-mockup" className="img-fluid" placeholder="blur" height={567} width={283} />
 							</div>
 							<div className="col-lg-6 col-xl-6 offset-xl-1 d-flex flex-column justify-content-center order-first order-lg-last">
 								{/* <h6 class="text-success">Features</h6> */}
