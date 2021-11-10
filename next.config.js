7386b26ec7ca368dad61f35b64d358685fa1d75a
Victor <vchweya@avicdesign.com>
2021-11-10 11:33:32 +0300

    made modifications to postcss

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+const withPurgeCss = require("next-purgecss");
 module.exports = {
   reactStrictMode: true,
 	images: {
@@ -5,5 +6,8 @@ module.exports = {
 			'res.cloudinary.com'
 		],
 	},
+	
 }
-
+module.exports = withPurgeCss({
+    purgeCssEnabled: ({ dev, isServer }) => !dev && !isServer, // Only enable PurgeCSS for client-side production builds
+});
