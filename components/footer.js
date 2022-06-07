99bf61c848902f2ba5292702babab58fe72bde37
Victor <vchweya@avicdesign.com>
2022-06-07 10:22:27 +0300

    updated faqs and footer

--- components/footer.js
+++ components/footer.js
@@ -9,17 +9,17 @@ export default function Footer(){
 				<div className="container">
 					<hr />
 					<div className="row mt-5">
-						<div className="col-lg-8 d-flex flex-column flex-lg-row align-items-lg-center text-center">
+						<div className="col-lg-9 d-flex flex-column flex-lg-row align-items-lg-center text-center">
 							<Link href="/" passHref>
 								<a className="navbar-brand mb-2">
 									<Image src="/img/logo.svg" height={30} width={83} alt="Tulix" />
 								</a>
 							</Link>
 							
-							<p className="small mb-3 mb-lg-0">©2021 Tulix Inc. All rights reserved. An <a href="https://www.antler.co/" target="_blank" rel="noopener noreferrer">Antler</a> portfolio company. 
+							<p className="small mb-3 mb-lg-0">©2021 Tulix Inc. All rights reserved. An <a href="https://www.antler.co/" target="_blank" rel="noopener noreferrer">Antler</a> & <a href="https://www.antler.co/" target="_blank" rel="noopener noreferrer">Catalyst Fund</a> portfolio company. 
 							<Link href="/privacy-policy"><a> Privacy Policy</a></Link>  | <Link href="/terms-and-conditions"><a>Terms &amp; Conditions</a></Link></p>
 						</div>
-						<div className="col-lg-4  d-flex flex-column justify-content-lg-center align-items-lg-end text-center ">
+						<div className="col-lg-3  d-flex flex-column justify-content-lg-center align-items-lg-end text-center ">
 							<p className="mb-0">
 								<a href="https://facebook.com/TulixApp" role="link" aria-label="#" className="pl-lg-4" target="_blank" rel="noopener noreferrer"><Image src="/img/icons/facebook.svg" alt="facebook" width={16} height={16}/></a>
 								<a href="https://instagram.com/TulixApp" role="link" aria-label="#" className="pl-4" target="_blank" rel="noopener noreferrer"><Image src="/img/icons/instagram.svg" alt="instagram" width={16} height={16}/></a>
