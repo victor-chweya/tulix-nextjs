42ecc85f2f0cad50988e07530e07cb6335818a5c
Victor <vchweya@avicdesign.com>
2022-06-07 10:28:21 +0300

    updated footer

--- components/footer.js
+++ components/footer.js
@@ -16,11 +16,7 @@ export default function Footer(){
 								</a>
 							</Link>
 							
-<<<<<<< HEAD
-							<p className="small mb-3 mb-lg-0">©2022 Tulix Inc. All rights reserved. An <a href="https://www.antler.co/" target="_blank" rel="noopener noreferrer">Antler</a> portfolio company. 
-=======
 							<p className="small mb-3 mb-lg-0">©2021 Tulix Inc. All rights reserved. An <a href="https://www.antler.co/" target="_blank" rel="noopener noreferrer">Antler</a> & <a href="https://www.antler.co/" target="_blank" rel="noopener noreferrer">Catalyst Fund</a> portfolio company. 
->>>>>>> production
 							<Link href="/privacy-policy"><a> Privacy Policy</a></Link>  | <Link href="/terms-and-conditions"><a>Terms &amp; Conditions</a></Link></p>
 						</div>
 						<div className="col-lg-3  d-flex flex-column justify-content-lg-center align-items-lg-end text-center ">
