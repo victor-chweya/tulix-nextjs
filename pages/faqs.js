0df542fba184d386b9ff58cb07ff3ca27168ec68
Victor <vchweya@avicdesign.com>
2022-10-26 17:42:08 +0300

    done with faqs

--- pages/faqs.js
+++ pages/faqs.js
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Layout from "../components/layout";
 import FaqsGettingStarted from "./faqs-getting-started";
 import FaqsGeneral from "./faqs-general";
+import FaqsFeatures from "./faqs-features";
+import FaqsSupport from "./faqs-support";
 
 export default function Faqs() {
   const pageTitle = "FAQs - Support your loved ones from wherever you are.";
@@ -27,107 +29,106 @@ export default function Faqs() {
       <section className="about-us">
         <div className="container">
           <div className="row justify-content-lg-center ">
-            <div className="col-lg-8 pb-5">
+            <div className="col-lg-4">
               <h6 className="text-success">Help Centre</h6>
               <h2 className="section-title ">Frequently Asked Questions</h2>
             </div>
             <div className="col-lg-8 mb-5">
-              <div>
-                <ul
-                  className="nav nav-pills nav-fill mb-3"
-                  id="pills-tab"
-                  role="tablist"
-                >
-                  <li className="nav-item" role="presentation">
-                    <button
-                      className="nav-link active"
-                      id="pills-home-tab"
-                      data-toggle="pill"
-                      data-target="#pills-home"
-                      type="button"
-                      role="tab"
-                      aria-controls="pills-home"
-                      aria-selected="true"
-                    >
-                      Getting Started
-                    </button>
-                  </li>
-                  <li className="nav-item" role="presentation">
-                    <button
-                      className="nav-link"
-                      id="pills-profile-tab"
-                      data-toggle="pill"
-                      data-target="#pills-profile"
-                      type="button"
-                      role="tab"
-                      aria-controls="pills-profile"
-                      aria-selected="false"
-                    >
-                      General
-                    </button>
-                  </li>
-                  <li className="nav-item" role="presentation">
-                    <button
-                      className="nav-link"
-                      id="pills-contact-tab"
-                      data-toggle="pill"
-                      data-target="#pills-contact"
-                      type="button"
-                      role="tab"
-                      aria-controls="pills-contact"
-                      aria-selected="false"
-                    >
-                      Features
-                    </button>
-                  </li>
-                  <li className="nav-item" role="presentation">
-                    <button
-                      className="nav-link"
-                      id="pills-support-tab"
-                      data-toggle="pill"
-                      data-target="#pills-support"
-                      type="button"
-                      role="tab"
-                      aria-controls="pills-support"
-                      aria-selected="false"
-                    >
-                      Support
-                    </button>
-                  </li>
-                </ul>
-                <div className="tab-content" id="pills-tabContent">
-                  <div
-                    className="tab-pane fade show active"
-                    id="pills-home"
-                    role="tabpanel"
-                    aria-labelledby="pills-home-tab"
+              <ul
+                className="nav nav-pills nav-fill mb-3"
+                id="pills-tab"
+                role="tablist"
+              >
+                <li className="nav-item" role="presentation">
+                  <button
+                    className="nav-link active"
+                    id="pills-home-tab"
+                    data-toggle="pill"
+                    data-target="#pills-home"
+                    type="button"
+                    role="tab"
+                    aria-controls="pills-home"
+                    aria-selected="true"
                   >
-                    <FaqsGettingStarted />
-                  </div>
-                  <div
-                    className="tab-pane fade"
-                    id="pills-profile"
-                    role="tabpanel"
-                    aria-labelledby="pills-profile-tab"
+                    Getting Started
+                  </button>
+                </li>
+                <li className="nav-item" role="presentation">
+                  <button
+                    className="nav-link"
+                    id="pills-profile-tab"
+                    data-toggle="pill"
+                    data-target="#pills-profile"
+                    type="button"
+                    role="tab"
+                    aria-controls="pills-profile"
+                    aria-selected="false"
                   >
-                    <FaqsGeneral />
-                  </div>
-                  <div
-                    className="tab-pane fade"
-                    id="pills-contact"
-                    role="tabpanel"
-                    aria-labelledby="pills-contact-tab"
+                    General
+                  </button>
+                </li>
+                <li className="nav-item" role="presentation">
+                  <button
+                    className="nav-link"
+                    id="pills-contact-tab"
+                    data-toggle="pill"
+                    data-target="#pills-contact"
+                    type="button"
+                    role="tab"
+                    aria-controls="pills-contact"
+                    aria-selected="false"
                   >
                     Features
-                  </div>
-                  <div
-                    className="tab-pane fade"
-                    id="pills-support"
-                    role="tabpanel"
-                    aria-labelledby="pills-support-tab"
+                  </button>
+                </li>
+                <li className="nav-item" role="presentation">
+                  <button
+                    className="nav-link"
+                    id="pills-support-tab"
+                    data-toggle="pill"
+                    data-target="#pills-support"
+                    type="button"
+                    role="tab"
+                    aria-controls="pills-support"
+                    aria-selected="false"
                   >
                     Support
-                  </div>
+                  </button>
+                </li>
+              </ul>
+
+              <div className="tab-content" id="pills-tabContent">
+                <div
+                  className="tab-pane fade show active"
+                  id="pills-home"
+                  role="tabpanel"
+                  aria-labelledby="pills-home-tab"
+                >
+                  <FaqsGettingStarted />
+                </div>
+                <div
+                  className="tab-pane fade"
+                  id="pills-profile"
+                  role="tabpanel"
+                  aria-labelledby="pills-profile-tab"
+                >
+                  <FaqsGeneral />
+                </div>
+                <div
+                  className="tab-pane fade"
+                  id="pills-contact"
+                  role="tabpanel"
+                  aria-labelledby="pills-contact-tab"
+                >
+                  <FaqsFeatures />
+                </div>
+                <div
+                  className="tab-pane fade"
+                  id="pills-support"
+                  role="tabpanel"
+                  aria-labelledby="pills-support-tab"
+                >
+                  <FaqsSupport />
                 </div>
               </div>
             </div>
