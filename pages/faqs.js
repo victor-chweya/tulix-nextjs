ebc16c5b4a08cb5fb366b579bf6f825858362e86
Victor <vchweya@avicdesign.com>
2022-11-22 07:56:03 +0300

    added how it works page, updated faqs"

--- pages/faqs.js
+++ pages/faqs.js
@@ -27,109 +27,114 @@ export default function Faqs() {
         <title>{pageTitle}</title>
       </Head>
       <section className="about-us">
-        <div className="container">
-          <div className="row justify-content-lg-center ">
-            <div className="col-lg-8">
+        <div className="container ">
+          <div className="row justify-content-center">
+            <div className="col-lg-10 mb-5">
               <h6 className="text-success">Help Centre</h6>
               <h2 className="section-title ">Frequently Asked Questions</h2>
             </div>
 
-            <div className="col-lg-8 my-4">
-              <ul
-                className="nav nav-pills nav-fill mb-4"
-                id="pills-tab"
-                role="tablist"
-              >
-                <li className="nav-item" role="presentation">
-                  <button
-                    className="nav-link active"
-                    id="pills-home-tab"
-                    data-toggle="pill"
-                    data-target="#pills-home"
-                    type="button"
-                    role="tab"
-                    aria-controls="pills-home"
-                    aria-selected="true"
+            <div className="col-lg-10 my-5">
+              <div className="row">
+                <div className="col-lg-3">
+                  <ul
+                    className="nav  flex-lg-column"
+                    id="pills-tab"
+                    role="tablist"
                   >
-                    Getting Started
-                  </button>
-                </li>
-                <li className="nav-item" role="presentation">
-                  <button
-                    className="nav-link"
-                    id="pills-profile-tab"
-                    data-toggle="pill"
-                    data-target="#pills-profile"
-                    type="button"
-                    role="tab"
-                    aria-controls="pills-profile"
-                    aria-selected="false"
-                  >
-                    General
-                  </button>
-                </li>
-                <li className="nav-item" role="presentation">
-                  <button
-                    className="nav-link"
-                    id="pills-contact-tab"
-                    data-toggle="pill"
-                    data-target="#pills-contact"
-                    type="button"
-                    role="tab"
-                    aria-controls="pills-contact"
-                    aria-selected="false"
-                  >
-                    Features
-                  </button>
-                </li>
-                <li className="nav-item" role="presentation">
-                  <button
-                    className="nav-link"
-                    id="pills-support-tab"
-                    data-toggle="pill"
-                    data-target="#pills-support"
-                    type="button"
-                    role="tab"
-                    aria-controls="pills-support"
-                    aria-selected="false"
-                  >
-                    Support
-                  </button>
-                </li>
-              </ul>
-
-              <div className="tab-content" id="pills-tabContent">
-                <div
-                  className="tab-pane fade show active"
-                  id="pills-home"
-                  role="tabpanel"
-                  aria-labelledby="pills-home-tab"
-                >
-                  <FaqsGettingStarted />
+                    <li className="nav-item" role="presentation">
+                      <a
+                        className="nav-link active"
+                        id="pills-home-tab"
+                        data-toggle="pill"
+                        data-target="#pills-home"
+                        type="button"
+                        role="tab"
+                        aria-controls="pills-home"
+                        aria-selected="true"
+                      >
+                        Getting Started
+                      </a>
+                    </li>
+                    <li className="nav-item" role="presentation">
+                      <a
+                        className="nav-link"
+                        id="pills-profile-tab"
+                        data-toggle="pill"
+                        data-target="#pills-profile"
+                        type="button"
+                        role="tab"
+                        aria-controls="pills-profile"
+                        aria-selected="false"
+                      >
+                        General
+                      </a>
+                    </li>
+                    <li className="nav-item" role="presentation">
+                      <a
+                        className="nav-link"
+                        id="pills-contact-tab"
+                        data-toggle="pill"
+                        data-target="#pills-contact"
+                        type="button"
+                        role="tab"
+                        aria-controls="pills-contact"
+                        aria-selected="false"
+                      >
+                        Features
+                      </a>
+                    </li>
+                    <li className="nav-item" role="presentation">
+                      <a
+                        className="nav-link"
+                        id="pills-support-tab"
+                        data-toggle="pill"
+                        data-target="#pills-support"
+                        type="button"
+                        role="tab"
+                        aria-controls="pills-support"
+                        aria-selected="false"
+                      >
+                        Support
+                      </a>
+                    </li>
+                  </ul>
                 </div>
-                <div
-                  className="tab-pane fade"
-                  id="pills-profile"
-                  role="tabpanel"
-                  aria-labelledby="pills-profile-tab"
-                >
-                  <FaqsGeneral />
-                </div>
-                <div
-                  className="tab-pane fade"
-                  id="pills-contact"
-                  role="tabpanel"
-                  aria-labelledby="pills-contact-tab"
-                >
-                  <FaqsFeatures />
-                </div>
-                <div
-                  className="tab-pane fade"
-                  id="pills-support"
-                  role="tabpanel"
-                  aria-labelledby="pills-support-tab"
-                >
-                  <FaqsSupport />
+                <div className="col-lg-9">
+                  <div className="tab-content" id="pills-tabContent">
+                    <div
+                      className="tab-pane fade show active"
+                      id="pills-home"
+                      role="tabpanel"
+                      aria-labelledby="pills-home-tab"
+                    >
+                      <FaqsGettingStarted />
+                    </div>
+                    <div
+                      className="tab-pane fade"
+                      id="pills-profile"
+                      role="tabpanel"
+                      aria-labelledby="pills-profile-tab"
+                    >
+                      <FaqsGeneral />
+                    </div>
+                    <div
+                      className="tab-pane fade"
+                      id="pills-contact"
+                      role="tabpanel"
+                      aria-labelledby="pills-contact-tab"
+                    >
+                      <FaqsFeatures />
+                    </div>
+                    <div
+                      className="tab-pane fade"
+                      id="pills-support"
+                      role="tabpanel"
+                      aria-labelledby="pills-support-tab"
+                    >
+                      <FaqsSupport />
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
