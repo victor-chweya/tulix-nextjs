dd4b1159f0bd0281f04de4301b13e2f69bb2613f
Victor Chweya <victor.chweya@mediapal.net>
2023-03-23 10:24:18 +0300

    updated cta padding

--- components/downloadCTA.js
+++ components/downloadCTA.js
@@ -6,14 +6,13 @@ export default function DownloadCTA() {
       <div className="d-flex flex-column flex-sm-row justify-content-center justify-content-md-start">
         <a
           href="https://apps.apple.com/ke/app/tulix/id1587163986"
-          className="btn bg-dark btn-lg  mr-sm-3 mr-lg-4 mb-3"
+          className="btn bg-dark btn-lg  mr-sm-3 mr-lg-4 mb-3 d-flex align-items-center justify-content-center"
           role="button"
           target="_blank"
           rel="noreferrer"
         >
           <Image
             src="/img/appstore-light.svg"
-            className="pt-1"
             width={124}
             height={33}
             alt="App Store"
@@ -22,14 +21,13 @@ export default function DownloadCTA() {
         </a>
         <a
           href="https://play.google.com/store/apps/details?id=com.solutions.tulix"
-          className="btn btn-dark btn-lg mb-3"
+          className="btn btn-dark btn-lg mb-3 d-flex align-items-center justify-content-center"
           role="button"
           target="_blank"
           rel="noreferrer"
         >
           <img
             src="/img/googleplay-light.svg"
-            className="pt-1"
             width={139}
             height={33}
             alt="Google Play"
